76f874e29edee8e99e69fdd0cac9d329d5e70628
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-17 20:50:37 +0300

    Adding functional search form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <div className="App"></div>;
+import Searchbar from "./components/Searchbar";
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+    querySearch: null,
+  };
+
+  handlerQuerySearch = (querySearch) => {
+    this.setState({ querySearch });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handlerQuerySearch} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -2,10 +2,33 @@ import { Component } from "react";
 import s from "./Searchbar.module.css";
 
 class Searchbar extends Component {
+  state = {
+    querySearch: "",
+  };
+
+  handlerQuerySearch = (event) => {
+    this.setState({
+      querySearch: event.currentTarget.value,
+    });
+  };
+
+  handlerSubmit = (event) => {
+    event.preventDefault();
+
+    if (this.state.querySearch.trim() === "") {
+      return alert("Enter query search");
+    }
+
+    this.props.onSubmit(this.state.querySearch);
+    this.setState({ querySearch: "" });
+  };
+
   render() {
+    const { querySearch } = this.state;
+
     return (
       <header className={s.header}>
-        <form className={s.form}>
+        <form className={s.form} onSubmit={this.handlerSubmit}>
           <button type="submit" className={s.button}>
             <span className={s.label}>Search</span>
           </button>
@@ -13,9 +36,11 @@ class Searchbar extends Component {
           <input
             className={s.input}
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
             placeholder="Search images and photos"
+            value={querySearch}
+            onChange={this.handlerQuerySearch}
           />
         </form>
       </header>
